41cb06db5eca8317732580dd99707d29abe1fb61
artem bystrov <ockidocki1@gmail.com>
2023-05-22 16:54:06 +0400

    fix added

--- src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.javaqadiplom;
-
-public class IllegalArgumentException extends RuntimeException {
-
-    public IllegalArgumentException (String msg) {
-        super (msg);
-    }
-}
-
